bb5fd48f828c68aa942815813865d92d8b6c7afc
TaoTePuh <github.com-2020-ttp@post42.de>
2025-04-27 20:05:10 +0200

    Add helpers.py with greet_user function

---
+++ utils/helpers.py
@@ -0,0 +1,7 @@
+# utils/helpers.py
+
+def greet_user(name: str) -> str:
+    """
+    Gibt einen Begrüßungstext für den angegebenen Namen zurück.
+    """
+    return f"Hallo {name}, willkommen in der traceability_app!"
