2690247178cb8ae0d23295da8b1cffc968ee4f16
TaoTePuh <github.com-2020-ttp@post42.de>
2025-05-08 01:54:59 +0200

    Erste Änderungen für das neue Bemerkungsfeld

--- forms.py
+++ forms.py
@@ -3,19 +3,22 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class UserForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=64)])
-    full_name = StringField('Vollständiger Name', validators=[Length(max=120)])
+    username        = StringField('Username', validators=[DataRequired(), Length(min=1, max=64)])
+    full_name       = StringField('Vollständiger Name', validators=[Length(max=120)])
+    remarks         = StringField('Bemerkungen', validators=[Length(max=256)])
     submit_redirect = SubmitField('Benutzer anlegen und auswählen')
-    submit_stay = SubmitField('Benutzer anlegen und weitere anlegen')
+    submit_stay     = SubmitField('Benutzer anlegen und weitere anlegen')
 
 class ProjectForm(FlaskForm):
-    name = StringField('Projektname', validators=[DataRequired(), Length(min=1, max=128)])
-    description = StringField('Beschreibung', validators=[Length(max=256)])
+    name            = StringField('Projektname', validators=[DataRequired(), Length(min=1, max=128)])
+    description     = StringField('Beschreibung', validators=[Length(max=256)])
+    remarks         = StringField('Bemerkungen', validators=[Length(max=256)])
     submit_redirect = SubmitField('Projekt anlegen und auswählen')
-    submit_stay = SubmitField('Projekt anlegen und weitere anlegen')
+    submit_stay     = SubmitField('Projekt anlegen und weitere anlegen')
 
 class MachineForm(FlaskForm):
-    name = StringField('Maschinenname', validators=[DataRequired(), Length(min=1, max=128)])
-    description = StringField('Beschreibung', validators=[Length(max=256)])
+    name            = StringField('Maschinenname', validators=[DataRequired(), Length(min=1, max=128)])
+    description     = StringField('Beschreibung', validators=[Length(max=256)])
+    remarks         = StringField('Bemerkungen', validators=[Length(max=256)])
     submit_redirect = SubmitField('Maschine anlegen und auswählen')
-    submit_stay = SubmitField('Maschine anlegen und weitere anlegen')
+    submit_stay     = SubmitField('Maschine anlegen und weitere anlegen')
--- models.py
+++ models.py
@@ -2,28 +2,31 @@ from extensions import db
 from datetime import datetime
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    full_name = db.Column(db.String(120), nullable=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    id          = db.Column(db.Integer, primary_key=True)
+    username    = db.Column(db.String(64), unique=True, nullable=False)
+    full_name   = db.Column(db.String(120), nullable=True)
+    created_at  = db.Column(db.DateTime, default=datetime.utcnow)
+    remarks     = db.Column(db.String(256), nullable=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
 
 class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), unique=True, nullable=False)
+    id          = db.Column(db.Integer, primary_key=True)
+    name        = db.Column(db.String(128), unique=True, nullable=False)
     description = db.Column(db.String(256))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at  = db.Column(db.DateTime, default=datetime.utcnow)
+    remarks     = db.Column(db.String(256), nullable=True)
 
     def __repr__(self):
         return f'<Project {self.name}>'
 
 class Machine(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), unique=True, nullable=False)
+    id          = db.Column(db.Integer, primary_key=True)
+    name        = db.Column(db.String(128), unique=True, nullable=False)
     description = db.Column(db.String(256))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at  = db.Column(db.DateTime, default=datetime.utcnow)
+    remarks     = db.Column(db.String(256), nullable=True)
 
     def __repr__(self):
         return f'<Machine {self.name}>'
